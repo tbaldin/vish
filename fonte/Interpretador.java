96ae8931a5d5f7fc1cb6163e446226fa4392457c
Régis Thiago Feyh <registhiagofeyh@gmail.com>
2014-04-25 22:41:28 -0300

    Implementada a estrutura inicial para verificação dos tokens. Verifica a primeira palavra da linha e identifica que tipo de comando virá. É preciso implementar a verificação da sintaxe agora de acordo com cada token encontrado.

--- fonte/Interpretador.java
+++ fonte/Interpretador.java
@@ -7,25 +7,40 @@
  * 
  * Por Régis Thiago Feyh <registhiagofeyh@gmail.com>
  */
-
+import java.util.Arrays;
 class Interpretador {
     private String linhas[];
-	private String tokens = "if while $";
+    private String[] tokens = {"if","var","while"};
+    private String[] comparadores = {"==",">","<","<>",">=","<=","!="};
+    
     public void interpreta(String l[]) {
+        int token;
+        String first,resto,arr[];
         this.linhas = l;
-        
         for(int i = 0; i < this.linhas.length; i++) {
             if(this.linhas[i] != null) {
-				String[] arr = this.linhas[i].split(" ",2);
-			    String first = arr[0];
-			    String resto = arr[1];
-			    System.out.println(first);
-			    if(1){ // condição para verificar se está nos tokens
-			    	System.out.println("Token encontrado");	
-			    } 
-                // TODO: interpretar a linha
-                System.out.println("Linha " + (i + 1) + ": " + this.linhas[i]);
-            }
-        }
-    }
+				arr = this.linhas[i].split(" ",2);
+				first = arr[0];
+				resto = arr[1];
+				token = Arrays.asList(this.tokens).indexOf(first);
+				if(token>=0){
+					switch(token){
+						case 0: // verificação de sintaxe se for condicional
+							System.out.println("Condicional if");
+							break;
+						case 1: // verificação de sintaze se for declaração de variável
+							System.out.println("Declaração de variável");
+							break;
+						case 2: // verificação de sintaxe se for laço
+							System.out.println("Laço while");
+							break;
+						default: break;
+					}
+				}else{
+					System.out.println("Não é token, verifica se é variável");
+				}
+				//System.out.println("Linha " + (i + 1) + ": " + this.linhas[i]);
+			}
+		}
+	}
 }
