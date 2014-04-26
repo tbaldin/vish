88576c09db66f8d3c05a93b6a9a1f4eafe50e6df
Régis Thiago Feyh <registhiagofeyh@gmail.com>
2014-04-25 21:36:00 -0300

    Adicionado código para converter a linha em um vetor de duas posições. Dessa maneira tenho a primeira palavra da linha. Falta verificar se é um token agora.

--- fonte/Interpretador.java
+++ fonte/Interpretador.java
@@ -10,12 +10,19 @@
 
 class Interpretador {
     private String linhas[];
-    
+	private String tokens = "if while $";
     public void interpreta(String l[]) {
         this.linhas = l;
         
         for(int i = 0; i < this.linhas.length; i++) {
             if(this.linhas[i] != null) {
+				String[] arr = this.linhas[i].split(" ",2);
+			    String first = arr[0];
+			    String resto = arr[1];
+			    System.out.println(first);
+			    if(1){ // condição para verificar se está nos tokens
+			    	System.out.println("Token encontrado");	
+			    } 
                 // TODO: interpretar a linha
                 System.out.println("Linha " + (i + 1) + ": " + this.linhas[i]);
             }
